39d5290dc87ff9cdb1ad2a13d36ee2dc34d764af
javascriptFan <wdeveloping9099@gmail.com>
2019-07-08 08:03:30 -0700

    implement new

--- client/app/routes/invoice/new.js
+++ client/app/routes/invoice/new.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model() {
+        return this.store.createRecord('invoice');
+    },
+
+    actions: {
+        createInvoice() {
+            let invoice = this.modelFor(this.routeName);
+            var self = this;
+            invoice.save().then(function(resp) {
+                self.transitionTo('/dashboard');
+            }).catch(function(reason) {
+                console.log(reason);
+            });
+        }
+    }
 });
