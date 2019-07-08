cec25cc20c88f88d378693db5b7a8f960575000c
javascriptFan <wdeveloping9099@gmail.com>
2019-07-08 07:57:08 -0700

    delete functionality

--- client/app/routes/dashboard.js
+++ client/app/routes/dashboard.js
@@ -2,8 +2,22 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return this.get('store').findAll('invoice');ß
+        return this.get('store').findAll('invoice');
     },
     actions: {
+        editInvoice(record) {
+            this.transitionTo('invoice.edit', record);
+        },
+        deleteInvoice(record) {
+            let confirmation = confirm('Are you sure you want to delete this invoice?');
+
+            if(confirmation) {
+                record.destroyRecord();
+
+                if(this.get('routeName') !== 'dashboard' ) {
+                    this.transitionTo('dashboard');
+                }
+            }
+        }
     }
 });
